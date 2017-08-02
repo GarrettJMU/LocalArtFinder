f5c919838adb1cfd3a6bbc00f2460204efbf4645
GarrettJMU <garretthughes3@gmail.com>
2017-08-02 15:34:54 -0700

    swipe setup and checkout functional

--- app/controllers/arts_controller.rb
+++ app/controllers/arts_controller.rb
@@ -64,6 +64,10 @@ class ArtsController < ApplicationController
     end
   end
 
+  def charges
+    redirect_to "charges/new"
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_art
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-
   get 'usertypes/index'
   get 'usertypes/route'
 
