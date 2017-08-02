58f187279f4cfced97ae9aa618a85cc029aee3e8
Matthieu Tripoli <tieum27@hotmail.com>
2017-08-02 14:04:39 -0700

    Added location finder to the Events Map

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -18,7 +18,36 @@ function initMap() {
     center: {lat: 32.7090852, lng: -117.1576205}, // Map centered on LEARN
     zoom: 15
   });
+  // Try HTML5 geolocation.
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    var pos = {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude
+    };
+
+    infoWindow.setPosition(pos);
+    infoWindow.setContent('Location found.');
+    infoWindow.open(map);
+    map.setCenter(pos);
+  }, function() {
+    handleLocationError(true, infoWindow, map.getCenter());
+  });
+} else {
+  // Browser doesn't support Geolocation
+  handleLocationError(false, infoWindow, map.getCenter());
 }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+infoWindow.setPosition(pos);
+infoWindow.setContent(browserHasGeolocation ?
+                      'Error: The Geolocation service failed.' :
+                      'Error: Your browser doesn\'t support geolocation.');
+infoWindow.open(map);
+}
+
+
 //   fetch('/events.json')
 //   .then(function(response){return response.json()})
 //   .then(plotMarkers);
