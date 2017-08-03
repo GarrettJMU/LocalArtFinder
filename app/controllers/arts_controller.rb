5b400349983cd4e1e910fb83cf31094c0d36ed6f
Mark <Mark@MTC.local>
2017-08-03 10:31:59 -0700

    added paperclip and views

--- app/controllers/arts_controller.rb
+++ app/controllers/arts_controller.rb
@@ -82,6 +82,6 @@ class ArtsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def art_params
-      params.require(:art).permit(:genre, :description, :price, :length, :width, :medium, :artist_id, :user_id)
+      params.require(:art).permit(:genre, :description, :price, :length, :width, :medium, :artist_id, :user_id, :image)
     end
 end
