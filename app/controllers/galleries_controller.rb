c68d649faf1b82cbc1e382f9c30b4f3260941c98
Garrett Hughes <garretthughes3@gmail.com>
2017-08-04 16:14:17 -0700

    Update galleries_controller.rb

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -2,7 +2,10 @@ class GalleriesController < ApplicationController
   before_action :set_gallery, only: [:show, :edit, :update, :destroy]
   # before_action :authenticate_user!
   # load_and_authorize_resource
-
+  # FIX THE AUTHENTICATE USER ONE 
+  
+  
+  
   # GET /galleries
   # GET /galleries.json
   def index
