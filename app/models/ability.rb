e23e452d65b3d5f480e160c1da6087b54f1992cf
Brianna Kutauskas <briannakutauskas@Briannas-MacBook-Pro.local>
2017-08-07 09:27:05 -0700

    first commit

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -10,9 +10,9 @@ class Ability
       can :manage, :all
     else
       can [:read, :create], :all
-      can :manage, Art, user_id: user.id
-      can :manage, Artist, user_id: user.id
-      can :manage, Event, user_id: user.id
+      can :manage, Art, :user_id => user.id
+      can :manage, Artist, :user_id => user.id
+      can :manage, Event, :user_id => user.id
     # Define abilities for the passed in user here. For example:
     end
 
--- app/models/art.rb
+++ app/models/art.rb
@@ -1,7 +1,7 @@
 class Art < ApplicationRecord
   belongs_to :artist
   belongs_to :user
-  #CHeck to make sure that the validates user: precesnse true isn't needed here
+  validates :user, presence: true
   has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200" }
   validates_attachment :image, presence: true,
     content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] },
