dc8b43de2aeaa07a066244a34da7b293e185ddc8
Mark <Mark@MTC.local>
2017-10-16 14:42:15 -0700

    css change for advanced search and dropdown for user menu, stripe payment updated to take variable, still needs finalization of last step to take in parameter for the id

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,11 +1,14 @@
 class ChargesController < ApplicationController
-  def new
+before_action :set_art, only: [:show, :edit, :update, :destroy]
+
+def new
 end
 
 def create
-  # Amount in cents
-  @amount = 500
-
+  # @amount = @art.price
+  # @url = request.original_url.to_s
+  # @url.split("/")
+  # @art = Art.find(@url[2])
   customer = Stripe::Customer.create(
     :email => params[:stripeEmail],
     :source  => params[:stripeToken]
@@ -13,13 +16,26 @@ def create
 
   charge = Stripe::Charge.create(
     :customer    => customer.id,
-    :amount      => @amount,
+    :amount      => @art.price * 100,
     :description => 'Rails Stripe customer',
-    :currency    => 'usd'
+    :currency    => 'usd',
+    :key         => Rails.application.secrets.stripe_publishable_key
   )
 
 rescue Stripe::CardError => e
   flash[:error] = e.message
   redirect_to new_charge_path
+end
+
+private
+
+# maybe not needed ///////////
+def set_art
+@art = Art.find(params[:id])
+end
+
+
+def charges_params
+  params.require(:art).permit(:id)
 end
 end
